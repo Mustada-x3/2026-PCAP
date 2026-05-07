66b83fa2fa9fffa0648ee946fd4ceced986b145b
Mustada-x3 <20261ctb0100029@estudantes.ifpr.edu.br>
2026-05-07 19:17:07 +0000

    1016.py

---
+++ BeeCrowd/1016.py
@@ -0,0 +1,15 @@
+'''
+Problema 1016 BeeCrowd
+Alice Ribeiro Marenda
+23.04.26
+'''
+#Objetivo: Calcular em quanto tempo o carro Y se destancia do carro X em certa quantidade de km
+
+#---Analise(LIAC)---
+#Entrada: um numero inteiro correspondente a distancia em km
+#Processamento: calcular  o tempo em minutos que levará para se formar a distância sugeridada entre os dois carros
+#Saida: exibir "resultado em minutos"
+
+km = int(input())
+d = km * 2 
+print(f"{d} minutos")
