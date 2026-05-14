1c3ae615c6cba26325683e58981d256a3e7c011b
Mustada-x3 <20261ctb0100029@estudantes.ifpr.edu.br>
2026-05-14 13:08:10 +0000

    1052.py

---
+++ BeeCrowd/1052.py
@@ -0,0 +1,37 @@
+'''
+Problema 1052 BeeCrowd
+Alice Ribeiro Marenda
+14.05.26
+'''
+#Objetivo: ler um valor inteiro de 1 a 12, e indicar corretamente o mês correspondente ao numero
+
+#---Analise(LIAC)---
+#Entrada: um unico valor inteiro
+#Processamento: indentificar o mês pertencente ao numero enviado
+#Saida: o mes correspondente ao numero da entrada
+
+n = int(input())
+if n == 1 :
+    print("Janeiro")
+elif n == 2 :
+    print("Fevereiro")
+elif n == 3 :
+    print("Março")
+elif n == 4 :
+    print("Abril")
+elif n == 5 :
+    print("Maio")
+elif n == 6 :
+    print("Junho")
+elif n == 7 :
+    print("Julho")
+elif n == 8 :
+    print("Agosto")
+elif n == 9 :
+    print("Setembro")
+elif n == 10 :
+    print("Outubro")
+elif  n == 11 :
+    print("Novembro")
+elif n == 12 :
+    print("Dezembro")
