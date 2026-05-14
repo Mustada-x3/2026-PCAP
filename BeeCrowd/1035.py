28797f66bcb7bac4607601c3a52c65218f96f9b2
Mustada-x3 <20261ctb0100029@estudantes.ifpr.edu.br>
2026-05-14 13:30:48 +0000

    1035.py

---
+++ BeeCrowd/1035.py
@@ -0,0 +1,20 @@
+'''
+Problema 1035 BeeCrowd
+Alice Ribeiro Marenda
+14.05.26
+'''
+#Objetivo: ler 4 valores inteiros(A, B, C, D), se B for maior que C e D for maior que A , a soma de C e D for maior do que a de A e B, sendo C e D positivos e A um numero par exibir "Valores aceitos", caso o contrario exibir "Valores nao aceitos"
+
+#---Analise(LIAC)---
+# Entrada: quatro valores inteiros
+# Processamento: determinar se B > C, D > A, D + C > A + B, D e C sendo numeros positivos e A é par
+# Saida: caso a analise seja correta exibir "Valores aceitos", caso o contrário exibir "Valores nao aceitos"
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+if (((B > C) and (D > A)) and ((A + B) < (D + C)) and ((C > 0) and (D > 0)) and (A % 2 == 0)) :
+    print("Valores aceitos")
+else: 
+    print("Valores nao aceitos")
