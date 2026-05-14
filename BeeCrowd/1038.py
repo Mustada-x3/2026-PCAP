7fd60e3d8f965ef1a2f53fc38b00b16bb26fe59c
Mustada-x3 <20261ctb0100029@estudantes.ifpr.edu.br>
2026-05-14 13:46:07 +0000

    1038.py

---
+++ BeeCrowd/1038.py
@@ -0,0 +1,29 @@
+'''
+Problema 1038 BeeCrowd
+Alice Ribeiro Marenda
+14.05.26
+'''
+#Objetivo: ler o codigo de um item de uma tabela de lanches e sua quantidade, em seguda calcular o total a ser gasto de acordo com a quantidade e o produto
+
+#---Analise(LIAC)---
+#Entrada: dois valores inteiros, um sendo o codigo do item e o outro a quantidade
+#Processamento: calcular o preço com base nos numeros de entrada
+#Saida: exibir "Total: R$" com duas casas após o ponto decimal
+
+c = int(input())
+q = int(input())
+if c == 1 :
+    n1 = 4.00 * q
+    print(f"Total: R$ {n1:.2f}")
+elif c == 2 :
+    n2 = 4.50 * q
+    print(f"Total: R$ {n2:.2f}")
+elif c == 3 :
+    n3 = 5.00 * q
+    print(f"Total R$ {n3:.2f}")
+elif c == 4 :
+    n4 = 2.00 * q 
+    print(f"Total: R$ {n4:.2f}")
+elif c == 5 :
+    n5 = 1.50 * q
+    print(f"Total: R$ {n5:.2f}")
